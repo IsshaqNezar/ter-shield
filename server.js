fcbb1f61894c9cf6534aaca4a26942fa267cc2c1
Isshaq Nezar <Isshaq1412@hotmail.fr>
2019-02-26 17:44:55 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser'); 
+
+//  create express app
+
+const app = express();
+
+//  parse requests of content-type - application
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+//  define a simple route
+app.get('/', (req,res) => {
+    res.json({"message": "Interface d'utilisateur"})
+});
+
+// listen for requests 
+app.listen(3000, () => {
+    console.log("Server is listening on port 3000");
+});
