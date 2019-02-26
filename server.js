244223a195f813169c7127c8dd9d865fca51390b
Isshaq Nezar <Isshaq1412@hotmail.fr>
2019-02-26 18:05:11 +0100

    First Commit

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser'); 
 
-//  create express app
+//  Création de app Express
 
 const app = express();
 
-//  parse requests of content-type - application
+//  requêtes d'analyse de type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// parse requests of content-type - application/json
+// requêtes d'analyse de type - application/json
 app.use(bodyParser.json())
 
-//  define a simple route
+// Configuration de la base de données 
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise
+
+// Connection à la base de données 
+mongoose.connect(dbConfig.url, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");
+}).catch(err => {
+    console.log('Echec de connexion à la base de données.', err);
+    process.exit();
+});
+
+//  définir une route simple
 app.get('/', (req,res) => {
     res.json({"message": "Interface d'utilisateur"})
 });
 
-// listen for requests 
+// Ecoute du port  
 app.listen(3000, () => {
     console.log("Server is listening on port 3000");
 });
