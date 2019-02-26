8fffbe99f643917c56d14f54d0ef92cfe4c567ad
Isshaq Nezar <Isshaq1412@hotmail.fr>
2019-02-26 18:29:00 +0100

    first commit

---
+++ config/database.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    url: 'mongodb://locoalhost:27017/ter-shield'
+}
+
--- server.js
+++ server.js
@@ -21,9 +21,9 @@ mongoose.Promise = global.Promise
 mongoose.connect(dbConfig.url, {
     useNewUrlParser: true
 }).then(() => {
-    console.log("Successfully connected to the database");
+    console.log("Successfully connected to the database");    
 }).catch(err => {
-    console.log('Echec de connexion à la base de données.', err);
+    console.log('Could not connect to the database. Exiting now...', err);
     process.exit();
 });
 
