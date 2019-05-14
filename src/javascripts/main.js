13d9831e9d2d6d0845b378456e0b5adb7dd867bb
Isshaq Nezar <Isshaq1412@hotmail.fr>
2019-05-14 11:55:06 +0200

    15

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,3 +1,4 @@
+
 $(function (){
 
     var donneePars;
@@ -87,72 +88,30 @@ $(function (){
                 type: 'POST',
                 url: '/data',
                 data: objetDonnee,
-                success: RecupData,
+                /* success: RecupData, */
                 error: error,
             });
 
         };
 
         function enregistrementAlea() {
-            
             aleatoire(0,20);
             saveData(alea);
             console.log('Test');
         };
 
-        
+        function timerfunc() {
+            var i =0;
+            while(i < 5) {
+                
+                setTimeout(enregistrementAlea,3000);
+                i++;
+            }
+            
+        };
+
+        timerfunc();
          /* enregistrementAlea();  */
-                    
-      
-
-        /* function reload(){
-            var container = document.getElementById("#donnees");
-            var content = container.innerHTML;
-            container.innerHTML= content; 
-            
-           //this line is to watch the result in console , you can remove it later	
-            console.log("Refreshed"); 
-        }; */
-
-       
-        /* testSocket.onmessage = function(event) {
-
-            var array = [], array2 = [];
-            var i = 0, b=0;
-            var count = 0, count2;
-            var labelSize= [];
-
-            afficherData(donnees);
-
-            tableau = donnees.map(function(val){
-                array = val.valeur;   
-                             
-                return array;
-                
-            });
-
-            i = tableau.length;
-            
-
-            for(count2 = i-30; count2<= i; count2++){
-
-                
-                array2[b] = tableau[count2];
-                
-                b++;
-            }
-
-            for (count = 0; count <= 30; count++){
-
-                labelSize[count] = count
-            
-            }
-              
-            dataChart.data.datasets[0].data = array2;
-            dataChart.data.labels = labelSize;
-            dataChart.update();     
-        
-        }; */
 
 
                /////////////////////////////// CHART //////////////////////////////
@@ -177,49 +136,9 @@ $(function (){
             dataChart.data.datasets[0].data = ordonnees;
             dataChart.data.labels = abscisses;
             dataChart.update();
-
-
-            /* var array = [], array2 = [];
-            var i = 0, b=0;
-            var count = 0, count2;
-            var labelSize= [];
-
-            afficherData(donnees);
-
-            tableau = donnees.map(function(val){
-                array = val.valeur;   
-                             
-                return array;
-                
-            });
-
-            i = tableau.length;
-            
-
-            for(count2 = i-30; count2<= i; count2++){
-
-                
-                array2[b] = tableau[count2];
-                
-                b++;
-            }
-
-            for (count = 0; count <= 30; count++){
-
-                labelSize[count] = count
-            
-            }
-              
-            UpdateData(array2, labelSize); */
                    
         }
         
-        /* function UpdateData(array2, labelSize) {
-            dataChart.data.datasets[0].data = array2;
-            dataChart.data.labels = labelSize;
-            dataChart.update();
-        } */
-
-           
+                
 
 });
