77e5ed9c8ea6988029edd5f423401587b58e63ed
mcedrdiego <cedricdiego0@gmail.com>
2019-06-05 10:59:18 +0200

    3ARDUINO

---
+++ Arduino/Arduino.ino
@@ -0,0 +1,126 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiServer.h>
+#include <WiFiUdp.h>
+#include <SPI.h>
+#include <WiFi.h>
+
+char ssid[] = "Diego2049";      //  your network SSID (name)
+char pass[] = "2049cediego";   // your network password
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+
+int status = WL_IDLE_STATUS;
+
+// Initialize the Wifi client library
+WiFiClient client;
+
+// server address:
+char server[] = "192.168.43.192";
+//IPAddress {192,168,43,192};
+
+unsigned long lastConnectionTime = 0;            // last time you connected to the server, in milliseconds
+const unsigned long postingInterval = 2L * 1000L; // delay between updates, in milliseconds
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv != "1.1.0") {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 5 seconds for connection:
+    delay(5000);
+  }
+  // you're connected now, so print out the status:
+  printWifiStatus();
+}
+
+void loop() {
+  // if there's incoming data from the net connection.
+  // send it out the serial port.  This is for debugging
+  // purposes only:
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
+
+  // if ten seconds have passed since your last connection,
+  // then connect again and send data:
+  if (millis() - lastConnectionTime > postingInterval) {
+    httpRequest();
+  }
+
+}
+
+// this method makes a HTTP connection to the server:
+void httpRequest() {
+  // close any connection before send a new request.
+  // This will free the socket on the WiFi shield
+  client.stop();
+  float val = analogRead(A15)*5.0/1024;
+  String PostData= "{\"valeur\":";
+  PostData+=val;
+  PostData+=",\"date\":\"";
+  PostData+=millis()/1000.0;
+  PostData+="\"}";
+
+  // if there's a successful connection:
+  if (client.connect(server, 5000)) {
+    Serial.println("connecting...");
+    // send the HTTP POST request:
+    client.println("POST /data HTTP/1.1");
+    client.print("Host: ");
+    client.println(server);
+    client.println("User-Agent: Arduino/1.1");
+    client.println("Connection: Keep-Alive");
+    client.println("Content-Type: application/json");
+    client.print("Content-Length:");
+    client.println(PostData.length());
+    client.println();
+    client.println(PostData);
+    client.println();
+
+    // note the time that the connection was made:
+    lastConnectionTime = millis();
+  } else {
+    // if you couldn't make a connection:
+    Serial.println("connection to server failed");
+  }
+}
+/**/
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
